c56fab3e6baebd45cc1a71c17a7b30c3cb14c756
kwrnya <y21kurnia@gmail.com>
2021-01-30 22:25:43 +0700

    refactoring code

--- yutub.py
+++ yutub.py
@@ -12,35 +12,29 @@ class clrs:
     BOLD = '\033[1m'
     ENDC = '\033[0m'
 
-def on_progress(stream, chunk, bytes_remaining):
-    filesize = stream.filesize
-    bytes_received = filesize - bytes_remaining
-    display_progress_bar(bytes_received, filesize)
-
-def display_progress_bar(bytes_received, filesize, ch='█', scale=0.55):
-    _, columns = get_terminal_size()
-    max_width = int(columns * scale)
-    filled = int(round(max_width * bytes_received / float(filesize)))
-    remaining = max_width - filled
-    bar = ch * filled + ' ' * remaining
-    percent = round(100.0 * bytes_received / float(filesize), 1)
-    text = ' ↳ |{bar}| {percent}%\r'.format(bar=bar, percent=percent)
-    sys.stdout.write(text)
-    sys.stdout.flush()
-
-def get_terminal_size():
-    rows, columns = os.popen('stty size', 'r').read().split()
-    return int(rows), int(columns)
-
-def file_path():
-    home = os.path.expanduser('~')
-    download_path = os.path.join(home, 'Downloads/Videos')
-    return download_path
-
-def get_resolution(the_video):
-    the_video = the_video.streams.filter(progressive = True)
-    list_quality = [stream.resolution for stream in the_video]
-    return list_quality
+def main():
+    print(clrs.BOLD + "Yutub downloader " + VER + clrs.ENDC)
+    print("YouTube Videos Options :")
+    print("1 - Single")
+    print("2 - Playlist")
+    print("---")
+    print("0 - Exit")
+    
+    try:
+        select = int(input("Option => "))
+    except:
+        print(clrs.ERROR + clrs.BOLD + "Error - There is no such Option. \n" + clrs.ENDC)
+        main()
+    
+    if select == SINGLE:
+        singleDownload()    
+    elif select == PLAYLIST:
+        playlistDownload()
+    elif select == EXIT:
+        quit()
+    else:
+        print(clrs.ERROR + clrs.BOLD + "Error - There is no such Option. \n" + clrs.ENDC)
+        main()
 
 def singleDownload():
     print(clrs.BOLD + "Downloaded Videos Path : " + file_path() + clrs.ENDC)
@@ -92,29 +86,35 @@ def playlistDownload():
     print ("\nVideos Saved.\nEnjoy Your Videos.\n")
     again = main()
 
-def main():
-    print(clrs.BOLD + "Yutub downloader " + VER + clrs.ENDC)
-    print("YouTube Videos Options :")
-    print("1 - Single")
-    print("2 - Playlist")
-    print("---")
-    print("0 - Exit")
-    
-    try:
-        select = int(input("Option => "))
-    except:
-        print(clrs.ERROR + clrs.BOLD + "Error - There is no such Option. \n" + clrs.ENDC)
-        main()
-    
-    if select == SINGLE:
-        singleDownload()    
-    elif select == PLAYLIST:
-        playlistDownload()
-    elif select == EXIT:
-        quit()
-    else:
-        print(clrs.ERROR + clrs.BOLD + "Error - There is no such Option. \n" + clrs.ENDC)
-        main()
+def get_resolution(the_video):
+    the_video = the_video.streams.filter(progressive = True)
+    list_quality = [stream.resolution for stream in the_video]
+    return list_quality
+
+def get_terminal_size():
+    rows, columns = os.popen('stty size', 'r').read().split()
+    return int(rows), int(columns)
+
+def file_path():
+    home = os.path.expanduser('~')
+    download_path = os.path.join(home, 'Downloads/Videos')
+    return download_path
+
+def on_progress(stream, chunk, bytes_remaining):
+    filesize = stream.filesize
+    bytes_received = filesize - bytes_remaining
+    display_progress_bar(bytes_received, filesize)
+
+def display_progress_bar(bytes_received, filesize, ch='█', scale=0.55):
+    _, columns = get_terminal_size()
+    max_width = int(columns * scale)
+    filled = int(round(max_width * bytes_received / float(filesize)))
+    remaining = max_width - filled
+    bar = ch * filled + ' ' * remaining
+    percent = round(100.0 * bytes_received / float(filesize), 1)
+    text = ' ↳ |{bar}| {percent}%\r'.format(bar=bar, percent=percent)
+    sys.stdout.write(text)
+    sys.stdout.flush()
 
 file_size = 0
 if __name__ == "__main__":
